77391c617b6e418a17f1942af438daaa35837223
W2AlharbiMe <w2.alharbi.me@gmail.com>
2021-12-11 14:02:47 +0300

    remove old js

--- src/public/assets/js/app.js
+++
@@ -1,2 +0,0 @@
-// Version 0.0.1, Specific to app page
-const from=document.querySelector("#from"),file=document.querySelector("#file"),_to=document.querySelector("#to");from.addEventListener("change",function(e){const{value:t}=this;file.setAttribute("accept",`.${t}`),file.removeAttribute("disabled")});
